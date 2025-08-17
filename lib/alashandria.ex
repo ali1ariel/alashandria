9bb483bd8ebc80870fa933b510ca2d70593dd45f
Alisson Machado <aamachado@inf.ufsm.br>
2025-08-16 22:36:58 -0300

    🔥 initial commit 🔥

---
+++ lib/alashandria.ex
@@ -0,0 +1,18 @@
+defmodule Alashandria do
+  @moduledoc """
+  Documentation for `Alashandria`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Alashandria.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/alashandria_test.exs
@@ -0,0 +1,8 @@
+defmodule AlashandriaTest do
+  use ExUnit.Case
+  doctest Alashandria
+
+  test "greets the world" do
+    assert Alashandria.hello() == :world
+  end
+end
