71d385fba171ca0791251ff05df0260369919b01
Alisson Machado <aamachado@inf.ufsm.br>
2025-08-23 22:25:19 -0300

    test: init book tests

---
+++ test/alashandria/library/book_test.exs
@@ -0,0 +1,32 @@
+defmodule Alashandria.Library.BookTest do
+  use ExUnit.Case, async: false
+
+  import Alashandria.Generator
+  import ExUnitProperties
+
+  alias Alashandria.Library.Author
+  alias Alashandria.LibraryHelper
+
+  setup do
+    %{author: generate(author())}
+  end
+
+  describe "creating books" do
+    test "creating book with valid author", %{author: author} do
+      book =
+        book(author_id: author.id)
+        |> generate()
+
+      loaded_book = Ash.load!(book, :author)
+
+      assert author.name == loaded_book.author.name
+    end
+
+    test "creating book with invalid author" do
+      assert_raise(Ash.Error.Invalid, fn ->
+        book(author_id: nil)
+        |> generate()
+      end)
+    end
+  end
+end
