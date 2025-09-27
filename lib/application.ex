6de6524a4c1619f6ddd75e7455307c1cce0ee6b9
Alisson Machado <aamachado@inf.ufsm.br>
2025-09-27 19:20:18 -0300

    feat: Migrate data layer to Mnesia and enhance search capabilities
    Migrate Author and Book resources from Ash.DataLayer.Ets to Ash.DataLayer.Mnesia, improving database operations. Add public attribute visibility and default values, and introduce new search features for books and authors with support for partial string matches, addressing limitations in Mnesia's query capabilities. Implement validation to check for author existence when creating books.

--- lib/application.ex
+++ lib/application.ex
@@ -2,6 +2,8 @@ defmodule Alashandria.Application do
   use Application
 
   def start(_type, _args) do
+    setup_mnesia()
+
     children = [
       {Bandit, plug: Alashandria.Endpoint, port: 4000}
     ]
@@ -9,5 +11,10 @@ defmodule Alashandria.Application do
     opts = [strategy: :one_for_one, name: Alashandria.Supervisor]
 
     Supervisor.start_link(children, opts)
+  end
+
+  defp setup_mnesia do
+    :mnesia.create_schema([node()])
+    Ash.DataLayer.Mnesia.start(Alashandria.Library)
   end
 end
